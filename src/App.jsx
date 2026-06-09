80bde0963ec1e9867022af150e627e5b094fc75a
agustina505 <agustinabelen.fontivero@estudiantes.unahur.edu.ar>
2026-06-09 07:35:57 -0300

    probando carrito y detalle del producto

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,49 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
 import NavBarEx from './components/NavBar';
 import Footer from './components/Footer';
 import Inicio from './pages/Inicio';
 import Productos from './pages/Productos';
+import DetalleProducto from './pages/DetalleProducto';
+import Carrito from './pages/Carrito';
 
 function App() {
+  //estado del carrito
+  const [carrito, setCarrito] = useState([]);
+
+  const agregarAlCarrito = (producto, cantidad) => {
+    setCarrito((prevCarrito) => {
+      const existe = prevCarrito.find((item) => item.id === producto.id);
+      if (existe) {
+        return prevCarrito.map((item) =>
+          item.id === producto.id
+            ? { ...item, cantidad: item.cantidad + cantidad }
+            : item
+        );
+      }
+      return [...prevCarrito, { ...producto, cantidad }];
+    });
+    alert(`Agregaste ${producto.nombre} al carrito`);
+  };
+
   return (
     <BrowserRouter>
       <NavBarEx />
       <div className="container mt-4">
         <Routes>
           <Route path="/" element={<Inicio />} />
-          <Route path="/productos" element={<Productos />} />
+          <Route 
+            path="/productos" 
+            element={<Productos agregarAlCarrito={agregarAlCarrito} />} 
+          />
+          <Route 
+            path="/producto/:id" 
+            element={<DetalleProducto agregarAlCarrito={agregarAlCarrito} />} 
+          />
+          <Route 
+            path="/carrito" 
+            element={<Carrito carrito={carrito} setCarrito={setCarrito} />} 
+          />
         </Routes>
       </div>
       <Footer />
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,47 +5,22 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 
-function NavBarEx() {
-    return (
-        <Navbar expand="lg" className="bg-body-tertiary">
-            <Container fluid>
-                <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
-                <Navbar.Toggle aria-controls="navbarScroll" />
-                <Navbar.Collapse id="navbarScroll">
-                    <Nav
-                        className="me-auto my-2 my-lg-0"
-                        style={{ maxHeight: '100px' }}
-                        navbarScroll
-                    >
-                        <Nav.Link href="#action1">Home</Nav.Link>
-                        <Nav.Link href="#action2">Link</Nav.Link>
-                        <NavDropdown title="Link" id="navbarScrollingDropdown">
-                            <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action4">
-                                Another action
-                            </NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action5">
-                                Something else here
-                            </NavDropdown.Item>
-                        </NavDropdown>
-                        <Nav.Link href="#" disabled>
-                            Link
-                        </Nav.Link>
-                    </Nav>
-                    <Form className="d-flex">
-                        <Form.Control
-                            type="search"
-                            placeholder="Search"
-                            className="me-2"
-                            aria-label="Search"
-                        />
-                        <Button variant="outline-success">Search</Button>
-                    </Form>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-    );
-}
+const NavBarEx = () => {
+  return (
+    <Navbar bg="dark" variant="dark" expand="lg">
+      <Container>
+        <Navbar.Brand href="/">Nevermore</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link href="/">Inicio</Nav.Link>
+            <Nav.Link href="/productos">Productos</Nav.Link>
+            <Nav.Link href="/carrito">Carrito</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
 
 export default NavBarEx;
